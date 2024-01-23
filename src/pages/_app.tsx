c8876b607de71d2f9055c58f82262b224b33211d
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-23 19:53:20 +0900

    feat: react-query 인스턴스 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = new QueryClient();
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <QueryClientProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </QueryClientProvider>
   );
 }
