3f5648894eb19c206b5b5658aae8495eef4f1d5e
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-24 21:12:26 +0900

    feat:  response interceptor 추가.

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -3,3 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+api.interceptors.response.use((response) => {
+  return response.data;
+});
