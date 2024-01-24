c9c59ff1843feb2de5190a7074a0ce47f2ad69f9
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-24 21:07:54 +0900

    fix: typo

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ghchart.rshah.org",
+        port: "443",
+      },
+    ],
+  },
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_SERVER_URL}/:path*`,
       },
     ];
   },
