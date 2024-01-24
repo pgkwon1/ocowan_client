a525156f94e139741fb6c1f09f4ed5f36ed2418a
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-24 21:11:59 +0900

    feat: redux provider 추가.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import Layout from "@/components/Layout";
+import { store } from "@/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const persistor = persistStore(store);
   const client = new QueryClient();
   return (
-    <QueryClientProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />;
-      </Layout>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <QueryClientProvider client={client}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
   );
 }
