4f3eaff5f6a0276764b1a073635fee0136b71138
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-29 21:46:29 +0900

    feat: 401 에러 핸들링 추가.

--- src/modules/ApiInstance.ts
+++ src/modules/ApiInstance.ts
@@ -1,7 +1,15 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  validateStatus: (status) => {
+    if (status === 401) {
+      Promise.reject(401);
+      return false;
+    }
+    return true;
+  },
 });
 
 api.interceptors.response.use((response) => {
