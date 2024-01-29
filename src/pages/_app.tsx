0d34613ca20dc3c2f5e4e03cce453490504da74e
pgkwon1@naver.com <pgkwon1@naver.com>
2024-01-29 21:46:12 +0900

    feat: 전역 에러 핸들링 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import Layout from "@/components/Layout";
 import { store } from "@/store/store";
 import "@/styles/globals.css";
+import { AxiosError } from "axios";
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { useRouter } from "next/navigation";
+import { MutationCache, QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const persistor = persistStore(store);
-  const client = new QueryClient();
+  const router = useRouter();
+  const client = new QueryClient({
+    mutationCache: new MutationCache({
+      onError(error) {
+        if (error instanceof AxiosError && error.response?.status === 401) {
+          router.push("/member/logout");
+        }
+      },
+    }),
+  });
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
