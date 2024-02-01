cdc4e6046d9c482e6dc074c534ae364f989fa963
pgkwon1@naver.com <pgkwon1@naver.com>
2024-02-01 18:25:08 +0900

    update: global toast 개선

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,6 +1,6 @@
 import { ReactElement, useState } from "react";
 
-let setToast: (message: string) => void;
+let setToast: (message: string, isError: boolean) => void;
 
 export default function Toast({ children }: { children: ReactElement }) {
   const [show, setShow] = useState(false);
@@ -8,11 +8,11 @@ export default function Toast({ children }: { children: ReactElement }) {
     isError: false,
     message: "",
   });
-  setToast = (message: string) => {
+  setToast = (message: string, isError: boolean = false) => {
     setShow(true);
     setAlert({
-      isError: true,
-      message: message,
+      isError,
+      message,
     });
     setTimeout(() => {
       setShow(false);
@@ -26,11 +26,17 @@ export default function Toast({ children }: { children: ReactElement }) {
     <>
       {show ? (
         <div
-          id="toast-success"
-          className="fixed bottom-5 left-5 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          id={`${alert.isError ? "toast-danger" : "toast-success"}`}
+          className="flex fixed bottom-2 left-4 items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
           role="alert"
         >
-          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+          <div
+            className={`inline-flex items-center justify-center flex-shrink-0 w-8 h-8 ${
+              alert.isError
+                ? "text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200"
+                : "text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200"
+            } rounded-lg dark:bg-green-800 dark:text-green-200`}
+          >
             <svg
               className="w-5 h-5"
               aria-hidden="true"
@@ -48,10 +54,9 @@ export default function Toast({ children }: { children: ReactElement }) {
             className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
             data-dismiss-target="#toast-success"
             aria-label="Close"
+            onClick={() => setShow(false)}
           >
-            <span className="sr-only" onClick={() => setShow(false)}>
-              Close
-            </span>
+            <span className="sr-only">Close</span>
             <svg
               className="w-3 h-3"
               aria-hidden="true"
@@ -76,6 +81,6 @@ export default function Toast({ children }: { children: ReactElement }) {
     </>
   );
 }
-export const setGlobalToast = (message: string) => {
-  setToast(message);
+export const setGlobalToast = (message: string, isError: boolean = false) => {
+  setToast(message, isError);
 };
