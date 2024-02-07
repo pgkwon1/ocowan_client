1da9a92787733672ed44ddea33020e5f22673280
pgkwon1@naver.com <pgkwon1@naver.com>
2024-02-07 23:16:00 +0900

    feat: logout 및 로고 추가.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,22 @@
+import { IRootReducer } from "@/store/reducer.dto";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+
 export default function Header() {
+  const { isLogin, login } = useSelector(
+    (state: IRootReducer) => state.githubReducer
+  );
   return (
     <div className="min-h-40 flex flex-col">
-      <header className="py-4 px-8">
-        <h1 className="text-3xl text-white font-bold">OCOWAN</h1>
+      <header className="py-4 px-8 flex justify-between items-center">
+        <img src="/images/logo.png" alt="OCOWAN Logo" className="w-40 h-auto" />
+        {isLogin && login ? (
+          <Link href="/member/logout">
+            <button className="text-3xl text-white font-bold">LOGOUT</button>
+          </Link>
+        ) : (
+          ""
+        )}
       </header>
     </div>
   );
