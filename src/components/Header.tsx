21e7f80d33d0c389559d93029ff19b71786d9fdc
pgkwon1@naver.com <pgkwon1@naver.com>
2024-02-08 17:52:07 +0900

    feat: logo img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <div className="min-h-40 flex flex-col">
       <header className="py-4 px-8 flex justify-between items-center">
-        <img src="/images/logo.png" alt="OCOWAN Logo" className="w-40 h-auto" />
+        <img src="/logo.png" alt="OCOWAN Logo" className="w-40 h-auto" />
         {isLogin && login ? (
           <Link href="/member/logout">
             <button className="text-3xl text-white font-bold">LOGOUT</button>
