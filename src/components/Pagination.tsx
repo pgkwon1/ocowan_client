64a87c81df33031cb400842ef8275c9c84121862
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-13 01:19:05 +0900

    feat: paginate 컴포넌트 추가.

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,32 @@
+import Pagination from "react-js-pagination";
+
+interface IPaginate {
+  page: number;
+  totalCount: number;
+  onPageChange: (page: number) => void;
+}
+export default function Paginate({
+  page,
+  totalCount,
+  onPageChange,
+}: IPaginate) {
+  return (
+    <>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={10}
+        totalItemsCount={totalCount}
+        pageRangeDisplayed={5}
+        onChange={onPageChange}
+        itemClass="px-3 py-1"
+        linkClass="hover:bg-gray-200 text-gray-700"
+        activeClass="bg-blue-600 text-white"
+        activeLinkClass="hover:bg-blue-600"
+        disabledClass="opacity-50 cursor-not-allowed"
+        innerClass="flex justify-center space-x-2 mt-4"
+        nextPageText={">"}
+        prevPageText={"<"}
+      />
+    </>
+  );
+}
