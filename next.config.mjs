ce4ea0385fecddff2163757ecbdcf94ad51151b7
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-13 02:06:48 +0900

    update: azure blob storage image access url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+
+      {
+        protocol: "https",
+        hostname: "ocowanstorage.blob.core.windows.net",
+      },
     ],
   },
 
