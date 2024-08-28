265fe13fff209d4bc301d09a5546d048954e31ff
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-28 17:46:43 +0900

    fix: meta 태그 head 섹션 밖으로 나가는 부분 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <meta
-        name="google-site-verification"
-        content="upC0HEZqSTbD6EzT_b6xTrxsr8zYxFM982wGvWiVlsw"
-      />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="upC0HEZqSTbD6EzT_b6xTrxsr8zYxFM982wGvWiVlsw"
+        />
+      </Head>
       <body className="min-h-screen bg-gradient-to-b from-blue-600 to-blue-400">
         <Main />
         <NextScript />
