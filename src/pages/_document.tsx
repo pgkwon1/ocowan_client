0830ef987c167cbfc7b262d291b7c7183a1442da
pgkwon1@naver.com <pgkwon1@naver.com>
2024-08-28 17:24:32 +0900

    feat: google search console 메타태그 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta
+        name="google-site-verification"
+        content="upC0HEZqSTbD6EzT_b6xTrxsr8zYxFM982wGvWiVlsw"
+      />
       <body className="min-h-screen bg-gradient-to-b from-blue-600 to-blue-400">
         <Main />
         <NextScript />
