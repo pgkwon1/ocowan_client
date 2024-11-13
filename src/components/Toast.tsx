77e6338f594627ecd8831f73dbfefff3b2cbc29d
pgkwon1@naver.com <pgkwon1@naver.com>
2024-11-13 22:04:57 +0900

    REFACTOR: JSX 속성명 수정

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -83,9 +83,9 @@ export default function Toast({ children }: { children: ReactElement }) {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
               />
             </svg>
