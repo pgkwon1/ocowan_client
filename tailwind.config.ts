bda27dec4451c28a12eb873af3bce5534b33552e
pgkwon1 <pgkwon1@naver.com>
2025-01-22 19:42:46 +0900

    STYLE: media-query 화면 크기 설정.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,12 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
