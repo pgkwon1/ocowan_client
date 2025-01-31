666efd0168f9a81928fae8205e0217ae5df0a648
pgkwon1 <pgkwon1@naver.com>
2025-01-31 18:36:15 +0900

    FIX: TIL 카테고리 추가.

---
+++ src/constants/til.constants.ts
@@ -0,0 +1,21 @@
+export const TILCATEGORIES = [
+  "전체",
+  "프로그래밍 언어",
+  "프레임워크 및 라이브러리",
+  "데이터베이스",
+  "도구 및 환경",
+  "클라우드 및 배포",
+  "알고리즘 및 자료구조",
+  "웹 개발",
+  "모바일 개발",
+  "보안",
+  "기타",
+];
+
+export enum TIL_EMOJIS {
+  thumbsUp = "thumbsUp",
+  heart = "heart",
+  smile = "smile",
+  fire = "fire",
+  idea = "idea",
+}
