36a43a838800fdaf40edf80d1b4d004891cd52c7
pgkwon1 <pgkwon1@naver.com>
2025-04-30 18:47:55 +0900

    FEAT: google adsense 스크립트 추가.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
           name="google-site-verification"
           content="upC0HEZqSTbD6EzT_b6xTrxsr8zYxFM982wGvWiVlsw"
         />
+        {process.env.NODE_ENV === "production" ? (
+          <script
+            async={true}
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1833827605424482`}
+            crossOrigin="anonymous"
+          />
+        ) : (
+          ""
+        )}
       </Head>
       <body className="min-h-screen bg-[#F5F5F5]">
         <Main />
