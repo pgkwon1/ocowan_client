58c1e9eae5d798f1905ade67323e2d71fa2f1aef
pgkwon1 <pgkwon1@naver.com>
2025-05-21 19:10:07 +0900

    FEAT:  토큰 저장소 변경으로 인한  로그아웃에서 로컬스토리지 -> 쿠키 삭제로 변경

--- src/pages/member/logout.tsx
+++ src/pages/member/logout.tsx
@@ -2,12 +2,16 @@ import { resetData } from "@/store/reducers/users.reducer";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-
+import Cookies from "js-cookie";
 export default function Logout() {
   const router = useRouter();
   const dispatch = useDispatch();
 
   useEffect(() => {
+    Cookies.remove("token", {
+      sameSite: "strict",
+      secure: true,
+    });
     dispatch(resetData());
     router.push("/");
   });
