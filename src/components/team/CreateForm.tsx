a6203735a2c21854a2e0e2283a6f1b291b44a2ce
pgkwon1 <pgkwon1@naver.com>
2025-05-26 23:40:41 +0900

    FIX: typo

--- src/components/team/CreateForm.tsx
+++ src/components/team/CreateForm.tsx
@@ -15,7 +15,7 @@ export default function TeamCreateForm() {
     name: "",
     leader: login,
     description: "",
-    logo: {} as File,
+    teamLogo: {} as File,
   });
 
   const createMutate = useMutation({
@@ -85,17 +85,17 @@ export default function TeamCreateForm() {
         <div className="mb-5">
           <label
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            htmlFor="file_input"
+            htmlFor="teamLogo"
           >
             로고
           </label>
           <input
             className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            id="file_input"
+            id="teamLogo"
             type="file"
             onChange={(e) => {
               if (e.target.files !== null) {
-                setFormData("logo", e.target.files[0]);
+                setFormData("teamLogo", e.target.files[0]);
               }
             }}
           />
