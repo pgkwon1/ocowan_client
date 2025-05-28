dee76064dda7db30ec5e39d1ee772d57d513c34a
pgkwon1 <pgkwon1@naver.com>
2025-05-28 16:05:45 +0900

    FIX: 메인 소개 문구 수정.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,9 +22,16 @@ export default function Home() {
     <>
       <>
         <div className="font-extrabold text-xl text-center box-decoration-slice text-white bg-gradient-to-r from-blue-600 to-purple-600 py-3 px-4 rounded-lg shadow-md mb-4">
-          <h1 className="text-2xl mb-2">개발자의 Daily 루틴</h1>
-          <p className="text-lg font-medium">
+          <h1 className="text-2xl mb-2">
             오늘도 코딩 완료 <span className="text-yellow-300">✓</span>
+          </h1>
+          <p className="text-lg font-medium">
+            깃허브 활동
+            <span className="font-bold text-underline">
+              (커밋, PR, 이슈 등)
+            </span>
+            을 추적해 오코완 달성 여부와 <br />
+            3대(커밋, PR, 이슈들) 건수를 측정할 수 있습니다.
           </p>
         </div>
         <ConditionalWrapper
