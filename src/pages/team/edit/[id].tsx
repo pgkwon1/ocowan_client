b17da6a66adc750c456a94a009c1958fd9f4a9b1
pgkwon1 <pgkwon1@naver.com>
2025-07-14 01:06:48 +0900

    FIX: typo

--- src/pages/team/edit/[id].tsx
+++ src/pages/team/edit/[id].tsx
@@ -122,7 +122,7 @@ export default function TeamEdit({ id }: { id: string }) {
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           onClick={async () => await editMutate.mutate()}
         >
-          팀 생성
+          팀 수정
         </button>
       </form>
     </div>
